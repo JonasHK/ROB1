e3d37a2654a0c66c609a97dc6d2ec69c0a629068
Sebastian <sebastian.ruge@gmail.com>
2020-12-22 13:27:08 +0100

    Imageprocessing node

---
+++ Project/imageProcessing.py
@@ -0,0 +1,19 @@
+# ROS Node
+from _future_ import print_function
+from beginner_tutorials.srv import ImageProcessing ImageProcessingResponse
+import rospy
+
+def handle_image_processing(req):
+    x_center_offset = 1
+    y_center_offset = 2
+    angle_offset = 3
+    color = 4
+    return ImageProcessingResponse(x_center_offset, y_center_offset, angle_offset, color)
+
+def image_processing_server():
+    rospy.init_node('image_processing_server')
+    s = rospy.Service('image_processing', ImageProcessing, handle_image_processing)
+    rospy.spin()
+
+if _name_ == "_main_":
+    image_processing_server()
