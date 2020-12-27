7695de0ff40c7c133b3d8d473b5dddd83a9c3a40
Jonas <jonas.h.kristensen@gmail.com>
2020-12-27 13:11:32 +0100

    Added seriallink with tool_length included

--- Aflevering 2/Simulation.m
+++ Aflevering 2/Simulation.m
@@ -2,7 +2,7 @@
 clear, close all;
 L1 = Revolute('d', 0, 'a', 0, 'alpha', pi/2)
 L2 = Revolute('d', 18, 'a', 62, 'alpha', pi)
-L3 = Revolute('d', 13, 'a', 58, 'alpha', pi)
+L3 = Revolute('d', 12, 'a', 58, 'alpha', pi)
 L4 = Revolute('d', 13, 'a', 0, 'alpha', -pi/2)
 L5 = Revolute('d', 12, 'a', 0, 'alpha', -pi/2)
 L6 = Revolute('d', 6, 'a', 0, 'alpha', 0)
@@ -12,12 +12,30 @@ figure()
 ur.teach()
 ur.fkine([0, 0.1, 0.2, pi/2, pi/4, 0])
 
+%% UR10 - UR10 fra nettet DH-parametre inkl. tool_length
+clear, close all;
+tool_length = 0.15; % m
+L1 = Revolute('d', 0.12, 'a', 0, 'alpha', pi/2);
+L2 = Revolute('d', 0, 'a', -0.612, 'alpha', 0);
+L3 = Revolute('d', 0, 'a', -0.5723, 'alpha', 0);
+L4 = Revolute('d', 0.163941, 'a', 0, 'alpha', pi/2);
+L5 = Revolute('d', 0.1157, 'a', 0, 'alpha', -pi/2);
+L6 = Revolute('d', 0.0922+tool_length, 'a', 0, 'alpha', 0);
+ur = SerialLink([L1, L2, L3, L4, L5, L6]);
+
+qr = [pi -0.35 1.2 pi/2 pi/4 0];
+figure(1)
+T = ur.fkine(qr)
+ur.plot(qr);
+%ur.teach()
+%ur.ikine(T)
+
 
 %% Forward Kinematic
 qn = [0 0.1 0.2 pi/2 pi/4 0];
 T1 = ur.fkine(qn)
 ur.plot(qn); %, 'workspace', [-10 10 -10 10 -10 10])
-ur.teach()
+%ur.teach()
 
 
 %% Testing Inverse Kinematics
@@ -25,9 +43,8 @@ ur.teach()
 qn2 = ur.ikine(T1)
 ur.plot(qn2); %, 'workspace', [-10 10 -10 10 -10 10])
 ur.teach()
-
-
-
+qn
+qn2
 %% Generate path
 xarr = -20:1:50; % y-values
 yarr = -25*ones(size(xarr)); % x-values (constant)
